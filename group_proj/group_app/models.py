f420abf5b72bbbab41ebd05094ac518bbc1a9ae9
Lily Martinez <lilimar@aol.com>
2021-01-30 13:59:29 -0600

    initial commit

--- group_proj/group_app/models.py
+++ group_proj/group_app/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from datetime import datetime
+import re
+import bcrypt
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
-# Create your models here.
+
+class UserManager(models.Manager):
+    def reg_validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = 'First Name must be at least 2 characters'
+
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = 'Last Name must be at least 2 characters'
+        
+        if len(postData['email']) == 0:
+            errors['email'] = "Email is required"
+        elif not EMAIL_REGEX.match(postData['email']):    #check format of email
+            errors['email'] = 'Invalid Email Address'
+        
+        existing_user = User.objects.filter(email=postData['email'])
+        if len(existing_user) !=0:
+            errors['user'] = "Email already in use"
+        
+        if len(postData['password']) < 8:
+            errors['password'] = 'Password must be at least 8 characters'        
+        elif postData['password'] != postData['confirm_pw']:
+            errors['password'] = 'Password and Confirm Password must match'  #don't tell them which one is wrong!
+        
+        return errors
+      
+    def login_validator(self, postData):
+        errors = {}
+        if len(postData['email']) == 0:
+            errors['email'] = 'Email is required'
+        elif not EMAIL_REGEX.match(postData['email']):    #check format of email
+            errors['email'] = 'Invalid Email Address'
+    
+        existing_user = User.objects.filter(email=postData['email'])
+        if len(existing_user) !=1:      #now checking that at least one is found
+            errors['email'] = "User not found"
+        else:
+            if len(postData['password']) < 8:
+                errors['password'] = 'Password must be at least 8 characters' 
+                
+            else :    
+                if bcrypt.checkpw(postData['password'].encode(), existing_user[0].password.encode()) != True:  #authenticate
+                    errors['email'] = 'Email and Password do not match'  #don't tell them which one is wrong!
+            
+        return errors
+    
+
+class User(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = UserManager()
+    
+#-------------------end of USER ---------------------------------------------------------
+
+#----add your new models here----
+#
+#---end of adding new models
+
+
+class Message(models.Model):    
+    msg_content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    msg_UsrJoin = models.ForeignKey(User, related_name="usermessages_join",on_delete=models.CASCADE)    #OneUserManyMessages
+    user_likes = models.ManyToManyField(User, related_name='liked_posts')                      #ManyUsersLikeManyMessages and ManyMessagesAreLikedbyManyUsers
+
+
+class Comment(models.Model):    
+    com_content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    com_UserJoin = models.ForeignKey(User, related_name="usercomments_join",on_delete=models.CASCADE)   #OneUserManyComments
+    msg_CommJoin = models.ForeignKey(Message, related_name="msgcomments_join",on_delete=models.CASCADE) #OneMsgManyComments
+
--- group_proj/group_app/views.py
+++ group_proj/group_app/views.py
@@ -6,9 +6,111 @@ from django.conf import settings    #map settings
 from datetime import datetime       #events/activities date -time
 
 
-
-def index(request):  
-    # context = {
-    #     #no models yet
-    # }
+def index(request): 
+    request.session.flush() 
     return render(request, 'index.html')
+
+def login(request):
+    if request.method == 'POST':
+        print(request.POST) #should see QueryDict
+        
+        errors = User.objects.login_validator(request.POST)
+        print(errors)
+        if len(errors)>0:
+            for key, value in errors.items():
+                messages.error(request, value)
+           
+            return render(request, 'partialMsgs.html')  #AJAX!!!        
+            #return redirect('/ABC')    #redirect the user back to the form to fix the errors
+        else:
+            
+            this_user = User.objects.get(email = request.POST['email'])   
+            request.session['user_id'] = this_user.id
+            #messages.success(request, "You have successfully logged in!")
+            return redirect('/ABC/myEvents')
+
+def regForm(request):
+    request.session.flush()
+    return render(request, 'regForm.html')
+
+def register(request):
+    if request.method == 'POST':
+        print(request.POST) #should see QueryDict
+        
+        errors = User.objects.reg_validator(request.POST)
+        if len(errors)>0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return render(request, 'partialMsgs.html')  #AJAX!!!
+            #return redirect('/ABC/regForm')    #redirect the user back to the form to fix the errors
+        else:
+            hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()    
+            new_user = User.objects.create(
+                first_name = request.POST['first_name'],
+                last_name = request.POST['last_name'],
+                email = request.POST['email'],
+                password = hashed_pw)       
+            request.session['user_id'] = new_user.id
+            #messages.success(request, "You have successfully registered!")
+            return redirect('/ABC/dashboard')
+
+
+def childForm(request):
+    request.session.flush()
+    return render(request, 'childForm.html')
+
+def regChild(request):
+    #see "register" -here it needs the Child model
+    return redirect('/ABC/myProfile') #when successful Register a Child is click redirect back to myProfile
+
+def myProfile(request): 
+    if 'user_id' not in request.session:
+        return redirect('/ABC')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+    }
+    return render(request, 'myProfile.html', context)
+
+def myEvents(request): 
+    if 'user_id' not in request.session:
+        return redirect('/ABC')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+    }
+    return render(request, 'myEvents.html', context)
+
+def dashboard(request): 
+    if 'user_id' not in request.session:
+        return redirect('/ABC')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+    }
+    return render(request, 'dashboard.html', context)
+
+def newJoin(request): 
+    if 'user_id' not in request.session:
+        return redirect('/ABC')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+    }
+    return render(request, 'newJoin.html', context)
+
+def confirmJoin(request): 
+    if 'user_id' not in request.session:
+        return redirect('/ABC')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+    }
+    return render(request, 'confirmJoin.html', context)
+
+
+def logout(request):
+    request.session.clear()
+    return redirect('/ABC')
+
+
