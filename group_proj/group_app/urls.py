6cec6f03d0a63782e231be0e697239fecbf0366f
jaehyun An <jaehyuna11@gmail.com>
2021-01-31 16:28:34 -0800

    update myprofile section + add property in models.py

--- group_proj/group_app/urls.py
+++ group_proj/group_app/urls.py
@@ -1,30 +1,30 @@
-from django.urls import path     
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-    #REMEMBER to call it as http://localhost:8000/ABC/
-   
-    #path('', views.maptest),	
-     path('', views.index),	#display login
-     path('login', views.login), #process/validate login
-     
-    path('regForm', views.regForm), #display regForm
-    path('register', views.register), #validate registration
-    
-    path('logout', views.logout), 
-    
-    #---------from here down -we need to work on the programming/development.  We also need to finish the models.py---------------
-    #I think there should be an Event model and a Child model -but we can discuss
-    
-    path('childForm', views.childForm), #display childForm
-    path('regChild', views.regChild), #validate registration for a Child
-    
-    path('myProfile', views.myProfile), 
-    path('myEvents', views.myEvents), 
-    path('dashboard', views.dashboard), 
-    path('newJoin', views.newJoin), 
+
+    # REMEMBER to call it as http://localhost:8000/ABC/
+
+    #path('', views.maptest),
+    path('', views.index),  # display login
+    path('login', views.login),  # process/validate login
+
+    path('regForm', views.regForm),  # display regForm
+    path('register', views.register),  # validate registration
+
+    path('logout', views.logout),
+
+    # ---------from here down -we need to work on the programming/development.  We also need to finish the models.py---------------
+    # I think there should be an Event model and a Child model -but we can discuss
+
+    path('childForm', views.childForm),  # display childForm
+    path('regChild', views.regChild),  # validate registration for a Child
+
+    path('myProfile', views.myProfile),
+    path('myEvents', views.myEvents),
+    path('dashboard', views.dashboard),
+    path('newJoin', views.newJoin),
     path('confirmJoin', views.confirmJoin),
-    
-       
+
+
 ]
